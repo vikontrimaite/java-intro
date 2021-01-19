c5a532fba014c34e2e568cd4e5cd4c81526b2cc3
Vilija Kontrimaite <vikontrimaite@gmail.com>
2021-01-19 15:13:19 +0200

    update HelloWorld function

--- 1-intro/src/HelloWorld.java
+++ 1-intro/src/HelloWorld.java
@@ -1,2 +1,7 @@
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Kuku");
+        System.out.println("Dvigubos kabutes \"Pavadinimas\" ");
+    }
 }
